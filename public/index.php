0fefa65daab24bb1af1017953938d2576d504ad1
gfxload <gfxload@gmail.com>
2025-03-25 09:35:40 -0400

    refactor index.php to improve application bootstrapping and error handling

--- public/index.php
+++ public/index.php
@@ -1,20 +1,54 @@
 <?php
 
-echo "Hello, world!";
-exit;
-
+use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
+/*
+|--------------------------------------------------------------------------
+| Check If The Composer Auto-Loader Exists
+|--------------------------------------------------------------------------
+|
+| If the composer auto-loader does not exist, then display an error
+| message and exit.
+|
+*/
+
+if (! file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    die('Could not open ' . __DIR__ . '/../vendor/autoload.php: No such file or directory');
 }
 
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
-    ->handleRequest(Request::capture());
+/*
+|--------------------------------------------------------------------------
+| Turn On The Lights
+|--------------------------------------------------------------------------
+|
+| This illuminates an incoming request and sends it down the application's
+| kernel.
+|
+*/
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request
+| through the kernel, and send the associated response back to
+| the client's browser allowing them to enjoy the creative
+| and wonderful application we have prepared for them.
+|
+*/
+
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
