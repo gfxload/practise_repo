e50aa38d6f42eefd927c7afb911be5f09377e0d3
gfxload <gfxload@gmail.com>
2025-03-25 09:39:41 -0400

    refactor index.php to streamline application bootstrapping and remove deprecated code

--- public/index.php
+++ public/index.php
@@ -1,54 +1,17 @@
 <?php
 
-use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-/*
-|--------------------------------------------------------------------------
-| Check If The Composer Auto-Loader Exists
-|--------------------------------------------------------------------------
-|
-| If the composer auto-loader does not exist, then display an error
-| message and exit.
-|
-*/
-
-if (! file_exists(__DIR__ . '/../vendor/autoload.php')) {
-    die('Could not open ' . __DIR__ . '/../vendor/autoload.php: No such file or directory');
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
 }
 
-require __DIR__ . '/../vendor/autoload.php';
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
 
-/*
-|--------------------------------------------------------------------------
-| Turn On The Lights
-|--------------------------------------------------------------------------
-|
-| This illuminates an incoming request and sends it down the application's
-| kernel.
-|
-*/
-
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
-| the client's browser allowing them to enjoy the creative
-| and wonderful application we have prepared for them.
-|
-*/
-
-$kernel = $app->make(Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+// Bootstrap Laravel and handle the request...
+(require_once __DIR__.'/../bootstrap/app.php')
+    ->handleRequest(Request::capture());
